42514c49c001e3fd71bb20dd60b3e7e14c4adb37
Keith Millar <keithmillar@hotmail.ca>
2020-06-13 23:27:52 +0000

    added tree methods

--- vampire.js
+++ vampire.js
@@ -10,21 +10,49 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVamps = 0;
+    let currentVamp = this;
 
+    // climb "up" the tree (using iteration), counting nodes, until no boss is found
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numberOfVamps++;
+    }
+    return numberOfVamps;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    // console.log(this.numberOfOffspring());
+    // console.log(vampire.numberOfOffspring());
+    // console.log(numberOfOffspring());
+    // console.log(vampire.numberOfOffspring());
+
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
+
+
+
+    // if (get this.numberOfOffspring() < get vampire.numberOfOffspring()) {
+    //   return true
+    // } else {
+    //    return false
+    // }
+
+   
 
   }
 
@@ -36,9 +64,8 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+  // TODO: tip - no loop!
   }
 }
 
 module.exports = Vampire;
-
